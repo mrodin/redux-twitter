550548bd0f863b7fd8948408bb212560f8c370a8
Martin Rodin <rodin.martin@gmail.com>
2018-09-27 13:58:20 +0200

    add tweets to the Redux store

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,14 +1,16 @@
 import { getInitialData } from '../utils/api';
 import { setAuthedUser } from './authedUser';
 import { receiveUsers } from './users';
+import { receiveTweets } from './tweets';
 
 const AUTHED_USER = 'tylermcginnis';
 
 export function handleInitialData() {
   return (dispatch) => {
     return getInitialData()
-      .then(({ users }) => {
+      .then(({ users, tweets }) => {
         dispatch(receiveUsers(users));
+        dispatch(receiveTweets(tweets));
         dispatch(setAuthedUser(AUTHED_USER));
       })
   }
