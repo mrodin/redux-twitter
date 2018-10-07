401128171bc1522a94dfb105fd120c25e8d49d29
Martin Rodin <rodin.martin@gmail.com>
2018-10-07 20:20:17 +0200

    add react-icons

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { formatTweet, formatDate } from '../utils/helpers';
+import { MdFavoriteBorder } from 'react-icons/md';
+import { MdReply } from 'react-icons/md';
 
 class Tweet extends Component {
   render() {
@@ -11,12 +13,16 @@ class Tweet extends Component {
         <div>
           <img src={avatar} className='tweet__avatar' alt="Author's avatar" />
         </div>
-        <div className='tweet__header'>
+        <div className='tweet__body'>
           <ul>
             <li className='tweet__username'>{name}</li>
             <li>{formatDate(timestamp)}</li>
-            <li>{text}</li>
           </ul>
+          <div className='mt--xs'>{text}</div>
+          <div className='mt--s'>
+            <MdFavoriteBorder style={iconStyle} />
+            <MdReply style={iconStyle} />
+          </div>
         </div>
       </div>
     )
@@ -34,5 +40,12 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
       : null
   }
 }
+
+const iconStyle = {
+  color: 'grey',
+  height: 28,
+  width: 28,
+  marginRight: 10
+};
 
 export default connect(mapStateToProps)(Tweet);
